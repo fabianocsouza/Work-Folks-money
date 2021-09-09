e3a8a3921f42710f1279e9a4b16b5ad9fc450f46
Fabiano Cardozo <fabiano1403@hotmail.com>
2021-09-08 22:07:26 -0300

    creating header and styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,16 @@
 import logoImg from '../../assets/logo.svg'
+
+import { Container, Content } from './styles'
+
 export function Header() {
   return (
-    <header>
-      <img src={logoImg} alt="work/folks-money" />
-      <button>
-        Nova transação
-      </button>
-    </header>
+    <Container>
+      <Content>
+        <img src={logoImg} alt="work/folks-money" />
+        <button>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
   )
 }
